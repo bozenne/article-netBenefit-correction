b05483e17871f572fa9432a420901adfa5f47b32
Brice Maxime Hugues Ozenne <hpl802@sund.root.ku.dk>
2020-07-14 10:27:48 +0200

    add R files for supplementary material

---
+++ SM-A-covarianceTerm.R
@@ -0,0 +1,124 @@
+## * Load packages
+library(lava)
+library(data.table)
+library(BuyseTest)
+library(ggplot2)
+library(pbapply)
+
+## * Simulation function
+m <- lvm()
+regression(m) <- Treatment ~ 1
+distribution(m,~Treatment) <- binomial.lvm(p=0.5)
+regression(m) <- t1 ~ x1+Treatment+rho*eta
+regression(m) <- t2 ~ x2+Treatment+rho*eta
+distribution(m,~t1) <- coxWeibull.lvm(scale=1)
+distribution(m,~t2) <- coxWeibull.lvm(scale=1)
+distribution(m,~cens1) <- coxWeibull.lvm(scale=0.25)
+distribution(m,~cens2) <- coxWeibull.lvm(scale=0.25)
+eventTime(m) <- time1 ~ min(t1=1,cens1=0)
+eventTime(m) <- time2 ~ min(t2=1,cens2=0)
+
+warper <- function(i, n, rho, model.tte = NULL){
+
+    d <- data.table::as.data.table(lava::sim(m, n = n, p = c(rho = rho)))
+    d[,Id := 1:.N]
+    d[,status1 := as.numeric(t1 <= cens1)]
+    d[,status2 := as.numeric(t2 <= cens2)]
+
+    ffFull <- Treatment ~ cont(t1, threshold = 0.5) + cont(t2, threshold = 0.5)
+    eFull.BT <- BuyseTest(ffFull,
+                          data = d, method.inference = "none", trace = 0)
+
+    ffCens <- Treatment ~ tte(time1, threshold = 0.5, status = status1) + tte(time2, threshold = 0.5, status = status2)
+    eCens.BT <- BuyseTest(ffCens, model.tte = model.tte,
+                          data = d, method.inference = "none", trace = 0)
+    eCensCorr.BT <- BuyseTest(ffCens, model.tte = model.tte,
+                              data = d, method.inference = "none", trace = 0, correction.uninf = 2)
+
+    allPairs <- expand.grid(index.x = d[Treatment == 1,Id],
+                            index.y = d[Treatment == 0,Id])
+    allPairs$tte1.x <- d[allPairs$index.x,t1]
+    allPairs$tte1.y <- d[allPairs$index.y,t1]
+    allPairs$tte2.x <- d[allPairs$index.x,t2]
+    allPairs$tte2.y <- d[allPairs$index.y,t2]
+
+    A <- (allPairs$tte2.x >= allPairs$tte2.y + 0.5) - (allPairs$tte2.y >= allPairs$tte2.x + 0.5)
+    B <- abs(allPairs$tte1.x - allPairs$tte1.y) < 0.5
+
+    return(list(covTime = cor(d$t1,d$t2),
+                covPair = cov(A,B),
+                full = eFull.BT,
+                censored = eCens.BT,
+                censoredCorr = eCensCorr.BT,
+                n = n,
+                rho = rho))
+}
+## * Run simulation
+n.sim <- 1000
+
+set.seed(10)
+ls.res <- c(pblapply(1:n.sim, warper, n = 500, rho=0),
+            pblapply(1:n.sim, warper, n = 500, rho=1),
+            pblapply(1:n.sim, warper, n = 500, rho=2),
+            pblapply(1:n.sim, warper, n = 500, rho=4),
+            pblapply(1:n.sim, warper, n = 500, rho=6))
+
+## * Process results
+#+BEGIN_SRC R :exports both :results output :session *R* :cache no
+M <- do.call(rbind,lapply(ls.res, function(iList){ ## iList <- ls.res[[1]]
+    dt1.full <- data.table(type = "full", endpoint = 1, rho = iList$rho, n = iList$n, 
+                           covTime = iList$covTime, covPair = iList$covPair,
+                           favorable = iList$full@count.favorable[1]/iList$full@n.pairs[1], 
+                           unfavorable = iList$full@count.unfavorable[1]/iList$full@n.pairs[1],
+                           delta = iList$full@delta[,1,"netBenefit"])
+    dt2.full <- data.table(type = "full", endpoint = 2, rho = iList$rho, n = iList$n,
+                           covTime = iList$covTime, covPair = iList$covPair,
+                           favorable = iList$full@count.favorable[2]/iList$full@n.pairs[1], 
+                           unfavorable = iList$full@count.unfavorable[2]/iList$full@n.pairs[1],
+                           delta = iList$full@delta[,2,"netBenefit"])
+
+    dt1.censored <- data.table(type = "censored", endpoint = 1, rho = iList$rho, n = iList$n,
+                               covTime = iList$covTime, covPair = iList$covPair,
+                               favorable = iList$censored@count.favorable[1]/iList$censored@n.pairs[1], 
+                               unfavorable = iList$censored@count.unfavorable[1]/iList$censored@n.pairs[1],
+                               delta = iList$censored@delta[,1,"netBenefit"])
+    dt2.censored <- data.table(type = "censored", endpoint = 2, rho = iList$rho, n = iList$n,
+                               covTime = iList$covTime, covPair = iList$covPair,
+                               favorable = iList$censored@count.favorable[2]/iList$censored@n.pairs[1], 
+                               unfavorable = iList$censored@count.unfavorable[2]/iList$censored@n.pairs[1],
+                               delta = iList$censored@delta[,2,"netBenefit"])
+
+    dt1.censoredCorr <- data.table(type = "censoredCorr", endpoint = 1, rho = iList$rho, n = iList$n,
+                                   covTime = iList$covTime, covPair = iList$covPair,
+                                   favorable = iList$censoredCorr@count.favorable[1]/iList$censoredCorr@n.pairs[1], 
+                                   unfavorable = iList$censoredCorr@count.unfavorable[1]/iList$censoredCorr@n.pairs[1],
+                                   delta = iList$censoredCorr@delta[,1,"netBenefit"])
+    dt2.censoredCorr <- data.table(type = "censoredCorr", endpoint = 2, rho = iList$rho, n = iList$n,
+                                   covTime = iList$covTime, covPair = iList$covPair,
+                                   favorable = iList$censoredCorr@count.favorable[2]/iList$censoredCorr@n.pairs[1], 
+                                   unfavorable = iList$censoredCorr@count.unfavorable[2]/iList$censoredCorr@n.pairs[1],
+                                   delta = iList$censoredCorr@delta[,2,"netBenefit"])
+
+    return(rbind(dt1.full,dt2.full,dt1.censored,dt2.censored,dt1.censoredCorr,dt2.censoredCorr))
+}))
+
+
+## * Display graph
+tableS <- M[,.(rep = .N, covTime = mean(covTime), covPair = mean(covPair) ,favorable = mean(favorable), unfavorable = mean(unfavorable), delta = mean(delta)),by = c("endpoint","type","rho")]
+vec.rho <- round(tableS[,.SD[1,covTime],by = "rho"][[2]],2)
+
+dt.gg <- melt(M, measure = c("favorable","unfavorable","delta","covPair"), id.vars = c("endpoint","type","rho"))
+dt.gg[(type != "full" | endpoint == 1) & variable == "covPair", value := NA]
+dt.gg[, rho := factor(rho, levels = unique(rho), labels = paste0(unique(rho),"\n (",vec.rho,")"))]
+dt.gg[, estimator := factor(type, c("full","censored","censoredCorr"), labels = c("complete data","Peron scoring rule","Peron scoring rule + correction at the pair level"))]
+dt.gg[, endpoint := factor(endpoint, levels = 1:2, labels = c("1st endpoint","2nd endpoint"))]
+## dt.gg[, variable := factor(variable, levels = c("favorable","unfavorable","delta"), labels = c(bquote(P[X >= Y+tau]),bquote(P[Y <= X+tau]),bquote(P[X >= Y+tau]-P[Y <= X+tau])))]
+dt.gg[, variable := factor(variable,
+                           levels = c("favorable","unfavorable","delta","covPair"),
+                           labels = c(bquote(P[X >= Y+tau]),bquote(P[Y <= X+tau]),bquote(P[X >= Y+tau]-P[Y <= X+tau]),bquote(Cov(.,.))))]
+gg <- ggplot(dt.gg, aes(x = rho, y = value, color = estimator))
+gg <- gg + geom_boxplot()
+gg <- gg + facet_grid(endpoint ~ variable)
+gg <- gg + theme(legend.position="bottom")
+gg <- gg + xlab("correlation coefficient (average empirical correlation between time 1 and time 2)")
+gg
---
+++ SM-D-analytic-formula.R
@@ -0,0 +1,111 @@
+calcNB <- function(lambdaC,
+                   lambda1, lambda2, lambda3, lambda4, lambda5,
+                   t1, t2, t3, t4){
+    delta1 <- t1
+    delta2 <- t2-t1
+    delta3 <- t3-t2
+    delta4 <- t4-t3
+
+    out <- setNames(rep(as.numeric(NA),7),
+                    c(t1,t2,t3,t4,"infty","total","netBenefit"))
+    out[1] <- 1-exp(-lambdaC*t1) + lambdaC/(lambdaC+lambda1)*(exp(-(lambdaC+lambda1)*t1)-1)
+
+    out[2] <- exp(-lambdaC*t1)-exp(-lambdaC*t2) + lambdaC*exp(-lambda1*delta1)*exp(lambda2*t1)/(lambdaC+lambda2)*(exp(-(lambdaC+lambda2)*t2)-exp(-(lambdaC+lambda2)*t1))
+    
+    out[3] <- exp(-lambdaC*t2)-exp(-lambdaC*t3) + lambdaC*exp(-lambda1*delta1-lambda2*delta2)*exp(lambda3*t2)/(lambdaC+lambda3)*(exp(-(lambdaC+lambda3)*t3)-exp(-(lambdaC+lambda3)*t2))
+
+    out[4] <- exp(-lambdaC*t3)-exp(-lambdaC*t4) + lambdaC*exp(-lambda1*delta1-lambda2*delta2-lambda3*delta3)*exp(lambda4*t3)/(lambdaC+lambda4)*(exp(-(lambdaC+lambda4)*t4)-exp(-(lambdaC+lambda4)*t3))
+
+    out[5] <- exp(-lambdaC*t4) - lambdaC*exp(-lambda1*delta1-lambda2*delta2-lambda3*delta3-lambda4*delta4)*exp(lambda5*t4)/(lambdaC+lambda5)*exp(-(lambdaC+lambda5)*t4)
+
+    out["total"] <- sum(out[1:5])
+    out["netBenefit"] <- 1-2*out["total"]
+    return(out)
+}
+
+HR <- 0.3
+lambdaC <- 0.01
+
+## ** early
+lambda1E <- lambdaC * HR
+lambda2E <- lambdaC * (0.25 + 0.75 * HR)
+lambda3E <- lambdaC * (0.5 + 0.5 * HR)
+lambda4E <- lambdaC * (0.75 + 0.25 * HR)
+lambda5E <- lambdaC * 1
+
+NBE <- calcNB(lambdaC = lambdaC,
+              lambda1 = lambda1E,
+              lambda2 = lambda2E,
+              lambda3 = lambda3E,
+              lambda4 = lambda4E,
+              lambda5 = lambda5E, 
+              t1 = 40,
+              t2 = 60,
+              t3 = 80,
+              t4 = 100)
+##         40         60         80        100      infty      total netBenefit 
+## 0.01777268 0.01853809 0.02407939 0.02812682 0.25742445 0.34594144 0.30811713 
+
+
+## FX <- function(t){
+##     out <- rep(NA,length(t))
+##     index1 <- which(t <= 40)
+##     index2 <- intersect(which(t <= 60), which(t > 40))
+##     index3 <- intersect(which(t <= 80), which(t > 60))
+##     index4 <- intersect(which(t <= 100), which(t > 80))
+##     index5 <- which(t > 100)
+
+##     out[index1] <- 1-exp(-lambda1E*t[index1])
+##     out[index2] <- 1-exp(-lambda1E*40-lambda2E*(t[index2]-40))
+##     out[index3] <- 1-exp(-lambda1E*40-lambda2E*20-lambda3E*(t[index3]-60))
+##     out[index4] <- 1-exp(-lambda1E*40-lambda2E*20-lambda3E*20-lambda4E*(t[index4]-80))
+##     out[index5] <- 1-exp(-lambda1E*40-lambda2E*20-lambda3E*20-lambda4E*20-lambda5E*(t[index5]-100))
+##     return(out)
+## }
+## fY <- function(t){
+##     lambdaC*exp(-lambdaC*t)
+## }
+
+## integrate(f = function(t){fY(t)*FX(t)}, lower = 0, upper = 40, subdivisions = 1000)
+## integrate(f = function(t){fY(t)*FX(t)}, lower = 40, upper = 60, subdivisions = 1000)
+## integrate(f = function(t){fY(t)*FX(t)}, lower = 60, upper = 80, subdivisions = 1000)
+## integrate(f = function(t){fY(t)*FX(t)}, lower = 80, upper = 100, subdivisions = 1000)
+## integrate(f = function(t){fY(t)*FX(t)}, lower = 100, upper = 1000, subdivisions = 1000)
+## integrate(f = function(t){fY(t)*FX(t)}, lower = 0, upper = 1000, subdivisions = 1000)
+
+## d <- simData(n.C = 10000,
+##              n.T = 10000,
+##              HR = 0.3,
+##              hazard.C = 0.01,
+##              hazard.censoring = 0.000000000000000000000001,
+##              type = "early")
+## e <- BuyseTest(group ~ cont(timetoevent, threshold = 0),
+##                method.inference = "u-statistic",
+##                data=d)
+## confint(e, statistic = "favorable")
+## confint(e, statistic = "unfavorable")
+
+
+
+## ** delayed
+lambda1D <- lambdaC * 1
+lambda2D <- lambdaC * (0.75 + 0.25 * HR)
+lambda3D <- lambdaC * (0.5 + 0.5 * HR)
+lambda4D <- lambdaC * (0.25 + 0.75 * HR)
+lambda5D <- lambdaC * HR
+
+NBD <- calcNB(lambdaC = lambdaC,
+              lambda1 = lambda1D,
+              lambda2 = lambda2D,
+              lambda3 = lambda3D,
+              lambda4 = lambda4D,
+              lambda5 = lambda5D, 
+              t1 = 40,
+              t2 = 60,
+              t3 = 80,
+              t4 = 100)
+##         40         60         80        100      infty      total netBenefit 
+## 0.05434444 0.04621729 0.04634688 0.04260991 0.23944854 0.42896705 0.14206590 
+
+######################################################################
+### code-analytic.R ends here
